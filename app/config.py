13698a6a9e786d289937dbc5d391660da1135a83
owenltm <owenlwiantoro01@gmail.com>
2024-08-25 20:11:38 +0800

    initial adding model

---
+++ app/config.py
@@ -0,0 +1,6 @@
+from langchain_community.chat_models import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(model = "Clawdia:latest")
+
